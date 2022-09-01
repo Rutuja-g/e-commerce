223929815dda069eeb0136f3b605f854c0df48f7
Rutuja Goythale <rutujarg29@outlook.com>
2022-09-01 19:59:06 +0530

    created custom hook for shows

---
+++ box-offices/.history/src/misc/custom-hooks_20220901193200.jsx
@@ -0,0 +1,22 @@
+import { useReducer, useEffect } from 'react';
+
+function showsReducer(prevState, action) {
+  switch (action.type) {
+    default:
+      return prevState;
+  }
+}
+
+function usePersistedReducer(reducer, initialState, key) {
+  const [state, dispatch] = useReducer(reducer, initialState, initial => {
+    const persisted = localStorage.getItem(key);
+    return persisted ? JSON.parse(persisted) : initial;
+  });
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(state));
+  }, [state, key]);
+  return [state.dispatch];
+}
+export function useShows(key = 'shows') {
+  return usePersistedReducer(showsReducer, [], key);
+}

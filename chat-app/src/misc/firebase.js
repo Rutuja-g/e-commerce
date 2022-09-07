f70de65271ae91f4c6de0bda617c4025835b6912
Rutuja Goythale <rutujarg29@outlook.com>
2022-09-07 18:24:41 +0530

    initialized firebase project

---
+++ chat-app/src/misc/firebase.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyBqrn0oH7e5g2RtJvBruAJ5M2vnuKEzRlc',
+  authDomain: 'chat-web-app-f2061.firebaseapp.com',
+  databaseURL: 'https://chat-web-app-f2061-default-rtdb.firebaseio.com',
+  projectId: 'chat-web-app-f2061',
+  storageBucket: 'chat-web-app-f2061.appspot.com',
+  messagingSenderId: '291437817362',
+  appId: '1:291437817362:web:d84202c29562a29b71f2cb',
+};
+
+const app = firebase.initializeApp(config);

ecc6c3a93ae31c4dd7511ae24c7f3409363e2edb
Rutuja Goythale <rutujarg29@outlook.com>
2022-10-05 21:31:36 +0530

    added useFilters component

---
+++ .history/src/components/FilterCheckbox_20221005212516.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {CustomInput} from 'reactstrap'
+
+const FilterCheckbox = ({id,name,checked,onChange,label}) => {
+  return (
+    <div>
+      <CustomIn
+    </div>
+  );
+};
+
+export default FilterCheckbox;
